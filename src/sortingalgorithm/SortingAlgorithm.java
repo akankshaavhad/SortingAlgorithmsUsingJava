9abe73efb5dd3a7e5e095251400e83494eff67ea
Akanksha1211 <akanksha.avhad12@gmail.com>
2017-03-29 15:27:17 -0400

    Implemented Insertion Sort

--- src/sortingalgorithm/SortingAlgorithm.java
+++ src/sortingalgorithm/SortingAlgorithm.java
@@ -12,11 +12,11 @@ package sortingalgorithm;
 public class SortingAlgorithm {
 
     /**
-     * @param args the command line arguments
+     * @param list containing integer value
      */
     public int[] bubbleSort(int[] list){
         int temp=0;
-        for(int i=0;i<=list.length-1;i++){
+        for(int i=0;i<list.length-1;i++){
             for(int j=0;j<list.length-1-i;j++){
                 if(list[j]>list[j+1]){
                     temp=list[j];
@@ -28,16 +28,40 @@ public class SortingAlgorithm {
         return list;
     }
     
+    public int[] insertionSort(int[] list){
+        int temp, key;
+        for(int i=1;i<list.length;i++){
+            key=list[i];
+            int j=i-1;
+            while(j<0 && key<list[j]){
+                    temp=list[j];
+                    list[j]=list[j+1];
+                    list[j+1]=temp;
+                    j--;
+            }
+            
+        }
+        return list;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         SortingAlgorithm sm=new SortingAlgorithm();
         int[] a={9,6,4,1, 7, 8, 3, 9};
-        int[] output=sm.bubbleSort(a);
-        for(int k=0;k<=output.length-1;k++){
-            System.out.print(output[k]);
+        int[] outputbubble=sm.bubbleSort(a);
+        System.out.print("Bubble Sort:- ");
+        for(int k=0;k<=outputbubble.length-1;k++){
+            System.out.print(outputbubble[k]);
             System.out.print(" ");
         }
-        
+        System.out.println();
+        System.out.println("=============================================");
+        int[] outputinsertion=sm.insertionSort(a);
+        System.out.print("Insertion Sort:- ");
+        for(int k=0;k<=outputinsertion.length-1;k++){
+            System.out.print(outputinsertion[k]);
+            System.out.print(" ");
+        }
     }
     
 }
